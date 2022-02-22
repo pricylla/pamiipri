24937979e944a3b14568d151a91872d0f6859ca7
pricylla <cyllars@gmail.com>
2022-02-21 22:43:59 -0300

    aula1

---
+++ olamundo.js
@@ -0,0 +1,13 @@
+console.log('ola mundo')
+var a = 2;
+console.log('esse é o conteúdo de a = ',a)
+var b = 3;
+console.log('esse é o resultado de ', a, '+',b, '=', a+b)
+var sub1 = 12;
+console.log('Esse é o valor 1')
+var sub2 = 2;
+console.log('Resultado da subtração',sub1,'-',sub2,'=',sub1-sub2)
+console.log('A multiplicação do conteúdo é',a,'*',b,'=',a*b)
+let nome = 'pri'
+console.log('Meu nome é ', nome)
+console.log(nome * a)//resulta o not is number(NaN)
